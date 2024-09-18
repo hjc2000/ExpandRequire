87ce199812dad1c76f53c4f1a988bc781ea6b3a0
hjc2000 <hjc.666@foxmail.com>
2024-09-18 15:22:34 +0800

    Console.WriteLine($"从环境变量 lua_libs 中得到 lua 库路径：{lua_libs_path}");

--- ExpandRequire/Program.cs
+++ ExpandRequire/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string? lua_libs_path = Environment.GetEnvironmentVariable("lua_libs");
+if (lua_libs_path is null)
+{
+	Console.WriteLine("不存在环境变量：lua_libs");
+}
+
+Console.WriteLine($"从环境变量 lua_libs 中得到 lua 库路径：{lua_libs_path}");
