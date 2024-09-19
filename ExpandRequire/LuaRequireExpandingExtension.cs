33fa0e2705f9a99c68caf7b2937aa275751f13cd
hjc2000 <hjc.666@foxmail.com>
2024-09-19 09:34:56 +0800

    main_file_content = main_file_content.AddWorkspaceFiles();

--- ExpandRequire/LuaRequireExpandingExtension.cs
+++ ExpandRequire/LuaRequireExpandingExtension.cs
@@ -5,6 +5,80 @@ namespace ExpandRequire;
 internal static class LuaRequireExpandingExtension
 {
 	/// <summary>
+	///		获取本可执行文件当前路径下的 main.lua 的内容。
+	/// </summary>
+	/// <returns></returns>
+	public static string GetMainFileContent()
+	{
+#if DEBUG
+		string main_file_path = "D:/repos/ElectricBatch/main.lua";
+#else
+		string main_file_path = "main.lua";
+#endif
+
+		using FileStream main_file = File.Open(main_file_path, FileMode.Open,
+			FileAccess.ReadWrite, FileShare.Read);
+
+		using StreamReader reader = new(main_file);
+		return reader.ReadToEnd();
+	}
+
+	/// <summary>
+	///		添加工作区中的其他 lua 文件到 main_lua_file_content 中。
+	/// </summary>
+	/// <param name="main_lua_file_content">main.lua 的内容</param>
+	/// <returns>返回 main_lua_file_content 添加了工作区中其他 lua 文件后的字符串。</returns>
+	public static string AddWorkspaceFiles(this string main_lua_file_content)
+	{
+		EnumerationOptions options = new()
+		{
+			RecurseSubdirectories = true,
+		};
+
+#if DEBUG
+		string workspace_directory = "D:/repos/ElectricBatch";
+#else
+		string workspace_directory = Directory.GetCurrentDirectory();
+#endif
+
+		IEnumerable<string> lua_file_paths = Directory.EnumerateFiles(workspace_directory, "*", options);
+
+		foreach (string path in lua_file_paths)
+		{
+			string? directory = Path.GetDirectoryName(path);
+			if (directory is null)
+			{
+				continue;
+			}
+
+			if (directory.EndsWith("out"))
+			{
+				// out 目录下的文件不被收集
+				continue;
+			}
+
+			if (path.EndsWith("main.lua"))
+			{
+				continue;
+			}
+
+			string? extension = Path.GetExtension(path);
+			if (extension is null || extension != ".lua")
+			{
+				// 排除不是 .lua 的文件
+				continue;
+			}
+
+			Console.WriteLine(path);
+			using FileStream fs = File.OpenRead(path);
+			using StreamReader reader = new(fs);
+			main_lua_file_content = $"{reader.ReadToEnd()}\r\n{main_lua_file_content}";
+		}
+
+		return main_lua_file_content;
+	}
+
+	/// <summary>
 	///		解析第一条 require 指令所导入的模块。
 	/// </summary>
 	/// <param name="lua_code_content"></param>
@@ -92,5 +166,19 @@ internal static class LuaRequireExpandingExtension
 
 		lua_code_content = lua_code_content.Trim();
 		return lua_code_content;
+	}
+
+	/// <summary>
+	///		将 lua 代码内容字符串写入输出文件 out.lua 中。
+	///		<br/>* out.lua 始终是新建的空白文件。
+	/// </summary>
+	/// <param name="lua_code_content"></param>
+	public static void Output(this string lua_code_content)
+	{
+		using FileStream out_file = File.Open("out.lua", FileMode.Create,
+			FileAccess.ReadWrite, FileShare.Read);
+
+		using StreamWriter writer = new(out_file);
+		writer.Write(lua_code_content);
 	}
 }
--- ExpandRequire/Program.cs
+++ ExpandRequire/Program.cs
@@ -1,53 +1,36 @@
 ﻿using ExpandRequire;
 
-EnumerationOptions options = new()
-{
-	RecurseSubdirectories = true,
-};
-
-foreach (string path in Directory.EnumerateFiles("D:\\repos\\ElectricBatch", "*", options))
-{
-	Console.WriteLine(path);
-}
-
-string lua_code_content = @"require(""Servo.EI"")
-require(""Servo.Param"")
-require(""Servo.EO"")
-require(""Servo.Core"")
-require(""Servo.Feedback"")
-require(""Servo.Mode7"")
-require(""Servo.Monitor"")
-require(""Detector.AccelerationDetector"")
-";
+string main_file_content = LuaRequireExpandingExtension.GetMainFileContent();
+main_file_content = main_file_content.AddWorkspaceFiles();
 
 // 已经导入过了的路径就放到这里，导入前查重，避免重复导入
 HashSet<string> imported_lua_path_set = [];
 
 while (true)
 {
-	string? lua_file_path = lua_code_content.ParseFirstRequiredModulePath();
+	string? lua_file_path = main_file_content.ParseFirstRequiredModulePath();
 	if (lua_file_path is null)
 	{
 		// 找不到 require 指令了
-		lua_code_content = lua_code_content.TrimEmptyLine();
-		Console.WriteLine(lua_code_content);
-		if (lua_code_content.Contains("require"))
+		main_file_content = main_file_content.TrimEmptyLine();
+		if (main_file_content.Contains("require"))
 		{
 			throw new Exception("未展开干净");
 		}
 
+		main_file_content.Output();
 		return;
 	}
 
 	// 仍然找得到 require 指令
-	lua_code_content = lua_code_content.RemoveFirstRequiredModule();
+	main_file_content = main_file_content.RemoveFirstRequiredModule();
 	if (!imported_lua_path_set.Contains(lua_file_path))
 	{
 		// 此路径的 lua 文件还没导入过
 		imported_lua_path_set.Add(lua_file_path);
 		using FileStream fs = File.OpenRead(lua_file_path);
 		using StreamReader sr = new(fs);
-		lua_code_content = $"{sr.ReadToEnd()}\r\n{lua_code_content}";
+		main_file_content = $"{sr.ReadToEnd()}\r\n{main_file_content}";
 	}
 }
 
