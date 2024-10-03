50004456a0c1233d5b577c77efa35a301f476ff1
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-10-03 19:08:09 +0800

    ExpandRequire

--- ExpandRequire/LuaRequireExpandingHelper.cs
+++ ExpandRequire/LuaRequireExpandingHelper.cs
@@ -1,4 +1,4 @@
-﻿using JCNET.字符串处理;
+﻿using JCNET.Lua;
 using System.Text;
 
 namespace ExpandRequire;
@@ -14,37 +14,18 @@ internal static class LuaRequireExpandingHelper
 	/// <exception cref="Exception"></exception>
 	public static void ExpandRequire()
 	{
+		string? lua_libs_path = Environment.GetEnvironmentVariable("lua_libs");
+		if (lua_libs_path is null)
+		{
+			throw new Exception("不存在环境变量：lua_libs");
+		}
+
 		string main_file_content = GetMainFileContent();
 		main_file_content = main_file_content.AddWorkspaceFiles();
-
-		// 已经导入过了的路径就放到这里，导入前查重，避免重复导入
-		HashSet<string> imported_lua_path_set = [];
-		while (true)
-		{
-			string? lua_file_path = main_file_content.ParseFirstRequiredModulePath();
-			if (lua_file_path is null)
-			{
-				// 找不到 require 指令了
-				main_file_content = main_file_content.TrimEmptyLine();
-				main_file_content = $"{main_file_content}\r\n";
-				main_file_content = main_file_content.SimplifyFunctionName();
-				main_file_content = $"local G={{}}\r\n{main_file_content}";
-				main_file_content.Output();
-				return;
-			}
-
-			// 仍然找得到 require 指令
-			main_file_content = main_file_content.RemoveFirstRequiredModule();
-			if (!imported_lua_path_set.Contains(lua_file_path))
-			{
-				// 此路径的 lua 文件还没导入过
-				imported_lua_path_set.Add(lua_file_path);
-				using FileStream fs = File.OpenRead(lua_file_path);
-				using StreamReader sr = new(fs);
-				main_file_content = $"{sr.ReadToEnd().RemoveComment()}\r\n{main_file_content}";
-				main_file_content = main_file_content.AddTitle(lua_file_path);
-			}
-		}
+		Console.WriteLine(main_file_content);
+		LuaCodeContent main = new(main_file_content, [lua_libs_path]);
+		main.ExpandRequire();
+		main.ToString().Output();
 	}
 
 	/// <summary>
@@ -63,9 +44,7 @@ internal static class LuaRequireExpandingHelper
 			FileAccess.ReadWrite, FileShare.Read);
 
 		using StreamReader reader = new(main_file);
-		string main_file_content = reader.ReadToEnd().RemoveComment();
-		main_file_content = main_file_content.AddTitle("main");
-		return main_file_content;
+		return reader.ReadToEnd();
 	}
 
 	/// <summary>
@@ -89,6 +68,7 @@ internal static class LuaRequireExpandingHelper
 		IEnumerable<string> lua_file_paths = Directory.EnumerateFiles(workspace_directory,
 			"*", options);
 
+		StringBuilder sb = new();
 		foreach (string path in lua_file_paths)
 		{
 			string? directory = Path.GetDirectoryName(path);
@@ -118,159 +98,11 @@ internal static class LuaRequireExpandingHelper
 			Console.WriteLine(path);
 			using FileStream fs = File.OpenRead(path);
 			using StreamReader reader = new(fs);
-			main_lua_file_content = $"{reader.ReadToEnd().RemoveComment()}\r\n{main_lua_file_content}";
-			main_lua_file_content = main_lua_file_content.AddTitle(path);
+			sb.AppendLine(reader.ReadToEnd());
 		}
 
-		return main_lua_file_content;
-	}
-
-	/// <summary>
-	///		删除注释
-	/// </summary>
-	/// <param name="lua_code_content"></param>
-	/// <returns></returns>
-	private static string RemoveComment(this string lua_code_content)
-	{
-		StringBuilder sb = new();
-		StringReader reader = new(lua_code_content);
-		while (true)
-		{
-			string? line = reader.ReadLine();
-			if (line is null)
-			{
-				break;
-			}
-
-			if (line.Trim().StartsWith("--"))
-			{
-				continue;
-			}
-
-			sb.AppendLine(line);
-		}
-
+		sb.AppendLine(main_lua_file_content);
 		return sb.ToString();
-	}
-
-	/// <summary>
-	///		解析第一条 require 指令所导入的模块。
-	/// </summary>
-	/// <param name="lua_code_content"></param>
-	/// <returns></returns>
-	private static string? ParseFirstRequiredModule(this string lua_code_content)
-	{
-		lua_code_content = lua_code_content.GetBetween(@"require(""", @""")").ToString();
-		if (lua_code_content != string.Empty)
-		{
-			return lua_code_content;
-		}
-
-		lua_code_content = lua_code_content.GetBetween(@"require('", @"')").ToString();
-		if (lua_code_content != string.Empty)
-		{
-			return lua_code_content;
-		}
-
-		return null;
-	}
-
-	/// <summary>
-	///		解析第一条 require 指令所导入的模块的路径。
-	/// </summary>
-	/// <param name="lua_code_content"></param>
-	/// <returns></returns>
-	/// <exception cref="Exception"></exception>
-	private static string? ParseFirstRequiredModulePath(this string lua_code_content)
-	{
-		string? module = ParseFirstRequiredModule(lua_code_content);
-		if (module is null)
-		{
-			return null;
-		}
-
-		module = module.Replace('.', '/');
-
-		string? lua_libs_path = Environment.GetEnvironmentVariable("lua_libs");
-		if (lua_libs_path is null)
-		{
-			throw new Exception("不存在环境变量：lua_libs");
-		}
-
-		module = $"{lua_libs_path}/{module}.lua";
-		return module;
-	}
-
-	/// <summary>
-	///		移除第一条 require 指令所导入的模块的所有 require 指令。
-	///		<br/>* 例如第一条 require 导入了模块 A，则除了会将第一条 require 移除意外，
-	///			   还会将其他地方的所有导入模块 A 的 require 移除掉。
-	/// </summary>
-	/// <param name="lua_code_content"></param>
-	/// <returns></returns>
-	private static string RemoveFirstRequiredModule(this string lua_code_content)
-	{
-		string? module = ParseFirstRequiredModule(lua_code_content);
-		if (module is null)
-		{
-			return lua_code_content;
-		}
-
-		CuttingMiddleResult cutting_middle_result = lua_code_content.CutMiddle($"require(\"{module}\")");
-		if (cutting_middle_result.Success)
-		{
-			lua_code_content = cutting_middle_result.Left.Trim()
-				+ "\r\n"
-				+ cutting_middle_result.Right.Trim();
-		}
-
-		cutting_middle_result = lua_code_content.CutMiddle($"require('{module}')");
-		if (cutting_middle_result.Success)
-		{
-			lua_code_content = cutting_middle_result.Left.Trim()
-				+ "\r\n"
-				+ cutting_middle_result.Right.Trim();
-		}
-
-		return lua_code_content;
-	}
-
-	/// <summary>
-	///		取出连续的 2 个空行和开头、结尾的空行。
-	/// </summary>
-	/// <param name="lua_code_content"></param>
-	/// <returns></returns>
-	private static string TrimEmptyLine(this string lua_code_content)
-	{
-		while (lua_code_content.Contains("\n\n\n"))
-		{
-			lua_code_content = lua_code_content.Replace("\n\n\n", "\n\n");
-		}
-
-		while (lua_code_content.Contains("\r\n\r\n\r\n"))
-		{
-			lua_code_content = lua_code_content.Replace("\r\n\r\n\r\n", "\r\n\r\n");
-		}
-
-		lua_code_content = lua_code_content.Trim();
-		return lua_code_content;
-	}
-
-	/// <summary>
-	///		给内容添加标题
-	/// </summary>
-	/// <param name="content"></param>
-	/// <param name="title"></param>
-	/// <returns></returns>
-	private static string AddTitle(this string content, string title)
-	{
-		content = "\r\n\r\n\r\n\r\n\r\n" +
-			$"------------------------------------------------------\r\n" +
-			$"-- {title}\r\n" +
-			$"------------------------------------------------------\r\n" +
-			$"{content}";
-
-		return content;
 	}
 
 	/// <summary>
